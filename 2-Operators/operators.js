721b65d7fef4e69bd32ab8b5d12b8d056034f44a
Bhumika Vadar <bhumikavadar8855@gmail.com>
2026-01-16 02:08:51 -0800

    Topic 2:Operators in JS

---
+++ 2-Operators/operators.js
@@ -0,0 +1,33 @@
+1.
+let n=5;
+console.log(n>0);
+2.
+let a=10,b=10;
+console.log(a===b);
+3.
+console.log(17%3);
+4.
+let x=4;
+console.log(x**2);
+5.
+let age=19;
+console.log(age>=18);
+6.
+let name="Bhumika";
+let score=95;
+console.log(name+"scored"+score);
+7.
+let username="admin";
+let password="123";
+console.log(username=="admin"&& password=="123");
+8.
+let f=100;
+let c=(f-32)*5/9;
+console.log(c);
+9.
+let marks=80;
+marks+=10;
+console.log(marks);
+10.
+let n1=7;
+console.log(n1>=1 && n1<=10);
