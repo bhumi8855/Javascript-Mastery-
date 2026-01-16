2646d8c843273da65f881bf514577382e3945f9a
Bhumika Vadar <bhumikavadar8855@gmail.com>
2026-01-16 03:19:56 -0800

    Mini Project:Interactive counter using DOM in JS

---
+++ 9-DOM/Interactive_counter.js
@@ -0,0 +1,13 @@
+let count=0;
+
+function increase(){
+    count++;
+
+document.getElementById("count").innerText=count;
+}
+
+function decrease(){
+    count--;
+
+document.getElementById("count").innerText=count;
+}
