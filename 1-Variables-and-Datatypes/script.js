6839561c8ca14bec66fa777a51f3d8e642618098
Bhumika Vadar <bhumikavadar8855@gmail.com>
2026-01-16 02:04:04 -0800

    Topic 1:Variables & datatypes

---
+++ 1-Variables-and-Datatypes/script.js
@@ -0,0 +1,39 @@
+
+   // 10 practice programs on variables and datatypes //
+     1.
+     let name = "Bhumika";
+     console.log("My name is "+name);
+     2.
+     let a=10;
+     let b=20;
+     console.log(a+b);
+     3.
+     let r=5;
+     let area=3.14*r*r;
+     console.log(area);
+     4.
+     let p=1000,r1=5,t=2;
+     console.log((p*r1*t)/100);
+     5.
+     let age=18;
+     console.log(age>=18);
+     6.
+     let first="Hello";
+     let second="JS";
+     console.log(first+second);
+     7.
+     let x=true;
+     console.log(typeof x);
+     8.
+     let c=30;
+     let f=(c*9/5)+32;
+     console.log(f);
+     9.
+     let x1=10,x2=20;
+     let temp=x1;
+     x1=x2;
+     x2=temp;
+     console.log(x1,x2);
+     10.
+     let city=prompt("Enter your city:");
+     console.log("You live in "+city);
