5e78ad08eb5d0cc02f80385a1e66e93bf2c6e4fa
Bhumika Vadar <bhumikavadar8855@gmail.com>
2026-01-16 02:07:00 -0800

    Mini Project:Simple calculator using variables in JS

---
+++ 1-Variables-and-Datatypes/calculator.js
@@ -0,0 +1,14 @@
+let num1=10;
+let num2=5;
+let add=num1+num2;
+let subtract=num1-num2;
+let multiply=num1*num2;
+let divide=num1/num2;
+
+console.log("Number 1:"+num1);
+console.log("Number 2:"+num2);
+
+console.log("Addition:"+add);
+console.log("Subtract:"+subtract);
+console.log("Multiply:"+multiply);
+console.log("Division:"+divide);
